dacd6b80ceacda9e59f11019e5554eb520914fdc
kp <panjian@ainirobot.com>
2018-11-20 20:35:36 +0800

    c

--- classics/geek_coin_change.py
+++ classics/geek_coin_change.py
@@ -3,20 +3,25 @@
 #https://practice.geeksforgeeks.org/problems/coin-change/0/?ref=self
 import sys
 
-def cc(total, coins, alls):
-    if total == 0:
-        return 0
-    for c in coins:
-        rest = total - c
-        if rest < 0:
-            continue
+def cc(total, coins):
+    re = [[total]]
+    for tmp in re:
+        rest = tmp[-1]
+        for c in coins:
+            t = tmp[:]
+            tc = rest - c
+            if tc >= 0:
+                t.append(tc)
+                re.append(t)
+    for x in re:
+        for i in range(len(x)):
 
 
-    pass
 
 case_num = int(sys.stdin.readline().strip())
 for i in range(case_num):
     _ = sys.stdin.readline().strip()
     coins = [int(x) for x in sys.stdin.readline().strip().split()]
     total = int(sys.stdin.readline().strip())
+    cc(total, coins)
     
