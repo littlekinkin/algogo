5ac2244fbea461b062e40a9d4f79a109a8ad42b9
宋英 <songying@songyingdeMacBook-Pro.local>
2019-03-06 23:09:37 +0800

    tree

---
+++ classics/tree.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from queue import Queue
+
+class Node():
+    def __init__(self, value=None, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+def pre_traverse(root):
+    if not root:
+        return
+    print(root.value)
+    pre_traverse(root.left)
+    pre_traverse(root.right)
+
+def pre_traverse4stack(root):
+    if not root:
+        return
+    temp = []
+    temp.append(root)
+    while temp:
+        now = temp.pop()
+        print(now.value)
+        if now.right:
+            temp.append(now.right)
+        if now.left:
+            temp.append(now.left)
+
+
+def mid_traverse(root):
+    if not root:
+        return
+    mid_traverse(root.left)
+    print(root.value)
+    mid_traverse(root.right)
+
+def post_traverse(root):
+    if not root:
+        return
+    post_traverse(root.left)
+    post_traverse(root.right)
+    print(root.value)
+
+
+if __name__ == '__main__':
+    root = Node('D',Node('B',Node('A'),Node('C')),Node('E',right=Node('G',Node('F'))))
+    pre_traverse(root)
+    print('--')
+    mid_traverse(root)
+    print('--')
+    post_traverse(root)
+    print('--')
+    pre_traverse4stack(root)
+
+
